f38194d3e1ad3c7964975cb228d41566c515e9b4
Erika Yoon <erikaleeyoon@gmail.com>
2020-05-18 13:02:58 -0700

    add personal twist

--- Chapter 4/commaCode.py
+++ Chapter 4/commaCode.py
@@ -1,6 +1,28 @@
-spam = ['apples','bananas', 'tofu', 'cats']
+# TASK:
 # Take a list value as an argument
-# Return a string with all the items separated by a comman and space
+# Return a string with all the items separated by a comma and space
     # With 'and' inserted before the last item
 # Ex: apples, bananas, tofu, and cats
 
+# PERSONAL TWIST:
+# If the first value of the list is a string
+    # Capitalize it
+
+### MY CODE BELOW: ###
+
+# Define function
+def str_list(list_value):
+    string = ''
+    for i in range(len(list_value)):
+        if i == 0 and type(list_value[i]) == str:
+            string = list_value[i].capitalize() + ', '
+        elif i == len(list_value) -1:
+            string = string + 'and ' + str(list_value[i])
+        else: string = string + str(list_value[i]) + ', '
+    print(string)
+
+# Test function with different values
+list1 = ['apples', 'bananas', 'tofu', 'cats']
+list2 = [1, 2, 3, 4, 5]
+list3 = [0, 'pi', 3.14, 'circles']
+str_list(list1)
