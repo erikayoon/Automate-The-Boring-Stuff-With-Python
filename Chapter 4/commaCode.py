e3cffe619b0f6b2e783e8b215ac1fd415cc5014b
Erika Yoon <erikaleeyoon@gmail.com>
2020-05-18 12:21:20 -0700

    add ch. 4 Comma Code

---
+++ Chapter 4/commaCode.py
@@ -0,0 +1,6 @@
+spam = ['apples','bananas', 'tofu', 'cats']
+# Take a list value as an argument
+# Return a string with all the items separated by a comman and space
+    # With 'and' inserted before the last item
+# Ex: apples, bananas, tofu, and cats
+
